2b1f3384d480901c7100d5b62b0d669fd9b68a85
Valentin <jkv21contact@gmail.com>
2025-03-20 18:18:56 -0500

    revise styling, flexbox, grid, learn dark mode and how to toggle it

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,87 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  // Add dark mode toggle
+  const [dark, setDark] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  // Toggle dark mode
+  const toggleDarkMode = () => {
+    setDark(!dark);
+  };
+
+  return (
+    <>
+      {/* First Class interactions */}
+      <div className="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-6 flex items-center justify-center">
+        <h1 className="text-center text-lg text-cyan-400 font-mono font-extrabold">
+          Hello World
+        </h1>
+      </div>
+
+      <br />
+
+      {/* Flexbox */}
+      <div className="flex flex-col items-center justify-center space-y-6 mt-2">
+        <div className="h-16 w-16 rounded-full bg-blue-500"></div>
+        <div className="h-16 w-16 rounded-full bg-orange-500"></div>
+        <div className="h-16 w-16 rounded-full bg-green-500"></div>
+      </div>
+
+      <br />
+
+      {/* Grid */}
+      <div className="grid grid-cols-3 gap-2 mt-2 mx-2">
+        <div className="h-16 rounded-full bg-blue-500"></div>
+        <div className="h-16 rounded-full bg-orange-500"></div>
+        <div className="h-16 rounded-full bg-green-500"></div>
+      </div>
+
+      <br />
+
+      {/* Responsiveness and media queries */}
+      <div className="block max-sm:bg-amber-500 max-md:bg-amber-700 mt-4">
+        <p className="text-white">I appear on screens larger than 768px</p>
+      </div>
+
+      <br />
+
+      {/* Implement dark mode */}
+      <div className="bg-white dark:bg-black dark:text-white">
+        Dark mode disabled
+      </div>
+
+      <br />
+
+      {/* JS Toggle dark mode */}
+      <div
+        className={`m-10 rounded-lg bg-white px-6 py-8 shadow-xl ring-1 ring-slate-900/5 dark:bg-black ${
+          dark && "dark"
+        }`}
+      >
+        <h3 className="text-base font-medium tracking-tight text-slate-900 dark:text-white">
+          Writes Upside-Down
+        </h3>
+        <p className="mt-2 text-sm text-slate-500 dark:text-blue-100">
+          The Zero Gravity Pen can be used to write in any orientation,
+          including upside-down. It even works in outer space.
+        </p>
+
+        <button
+          id="toggleDark"
+          className="px-4 py-2 text-sm font-medium mt-8 text-blue-900 bg-blue-100 rounded-md"
+          onClick={toggleDarkMode}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          Toggle Dark Mode
+        </button>
+      </div>
+
+      <br />
+
+      {/* Image component */}
+      
+    </>
   );
 }
